07784ccec5cbf5df9b12dac110170a831d2f5710
Hasan <hasangosain50@gmail.com>
2025-09-11 15:19:37 +0545

    Initial Commit for Assignment 2 Initialize Variables age,height,weight,grade,isStudent,name,salary,level and print them in console.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Example age
+        int age = 25;
+
+        // Example height in feet
+        float height = 5.9f;
+
+        // Example weight in kg
+        double weight = 70.5;
+
+        // Example grade   
+        char grade = 'A';
+
+        // Example boolean
+        bool isStudent = true;
+
+        // Example name
+        string name = "Hasan Gosain";
+
+        // Example salary
+        decimal salary = 50000.75m;
+
+        // Example level
+        byte level = 1;
+        
+
+        //display the variables
+        Console.WriteLine($"Name: {name}");// Using string interpolation
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Weight: " + weight);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Is Student: " + isStudent);
+        Console.WriteLine("Salary: " + salary);
+        Console.WriteLine("Level: " + level);
+        
+    }
+}
